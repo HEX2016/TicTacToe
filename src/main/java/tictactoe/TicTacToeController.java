03624d6da17730dba1c6891954e6c46cc3a38b85
Egill Gautur Steingrímsson <Egill Gautur Steingrmsson>
2016-10-19 16:43:16 +0000

    clean.bat og compile.bat

--- src/main/java/tictactoe/TicTacToeController.java
+++ src/main/java/tictactoe/TicTacToeController.java
@@ -1,7 +1,7 @@
 package tictactoe;
 public class TicTacToeController {
     public static void main(String[] args) {
-        
+        System.out.println("Hello world!!!");
     }
 
     public static String world() {
