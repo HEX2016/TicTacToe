1b81af596dae7f8aa6c358cd85675d8902b9483e
Sveinn Björnsson <sveinnb@islandsbanki.is>
2016-10-22 17:55:32 +0000

    main changes

--- src/main/java/tictactoe/Main.java
+++ src/main/java/tictactoe/Main.java
@@ -2,12 +2,13 @@ package tictactoe;
 
 public class Main{
 	
-	public static void main(string [] args){
-		System.out.println("Welcome human,\n This is the ultimate game of Tic Tac Toe.
-							\n\n If you are unprepared, be prepared.\nTwo players take turn inserting
-							two digits to the console, the valid digits are:\n\t '00','01','02'
-							\n\t '10','11','12'\n\t '20','21','22'
-							\n Lets start with player 1 which is 'X', then Player 2 which is 'O'");
-		TicTacToe.TicTacToe();
+	public static void main(String [] args){
+		System.out.println("Welcome human,\n This is the ultimate game of Tic Tac Toe."
+							+	"\n\n If you are unprepared, be prepared.\nTwo players take turn inserting"
+							+	"two digits to the console, the valid digits are:\n\t '00','01','02'"
+							+	"\n\t '10','11','12'\n\t '20','21','22'"
+							+	"\n Lets start with player 1 which is 'X', then Player 2 which is 'O'");
+		TicTacToe newGame = new TicTacToe();
+		
 	}
 }
